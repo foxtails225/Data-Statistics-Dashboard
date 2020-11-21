04cd48479920d8ba623c9fdab2f082be8c31bdbd
kwebdev225 <kwebdev225@gmail.com>
2020-11-21 11:00:39 +0800

    Add Build

---
+++ server/build/precache-manifest.e2c668bc6f56a6fe067e2b0f5ec52aca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c01184d1bfc0e1316734ae17e51850a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a11f79cf263a3ce04b16",
+    "url": "/static/css/main.c1244e4c.chunk.css"
+  },
+  {
+    "revision": "0f6be5eefda1c470232b",
+    "url": "/static/js/2.40669911.chunk.js"
+  },
+  {
+    "revision": "34a2f49502a86260e33e5624241697f4",
+    "url": "/static/js/2.40669911.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a11f79cf263a3ce04b16",
+    "url": "/static/js/main.574c2756.chunk.js"
+  },
+  {
+    "revision": "9f0aa13461a96d84e4d1",
+    "url": "/static/js/runtime-main.63c3063a.js"
+  }
+]);
