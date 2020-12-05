719db61d5d601e85501df086245912465c9976ea
kwebdev225 <kwebdev225@gmail.com>
2020-12-05 10:17:14 +0800

    Update package.json and add build folder

---
+++ server/build/precache-manifest.dffae63d516155585aff858f4f02fd7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "841a3db59dc9dcf1157b399861387bd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "623800a5454f42950bfd",
+    "url": "/static/css/main.c1244e4c.chunk.css"
+  },
+  {
+    "revision": "80b25c0ef2918e349e4d",
+    "url": "/static/js/2.ffdea602.chunk.js"
+  },
+  {
+    "revision": "dc2b2e3100b378145ebd3330c47497f0",
+    "url": "/static/js/2.ffdea602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "623800a5454f42950bfd",
+    "url": "/static/js/main.36e8eb5b.chunk.js"
+  },
+  {
+    "revision": "e64adae1c934d0454155",
+    "url": "/static/js/runtime-main.7c71aa63.js"
+  }
+]);
